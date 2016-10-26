22fea1edececd3eff001b9e208bfa1234f89503a
Pankaj Tandon <tandonp@upmc.edu>
2016-10-26 17:07:09 -0400

    Initial commit

---
+++ app/scripts/directives/header/header.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name ç.directive:adminPosHeader
+ * @description
+ * # adminPosHeader
+ */
+angular.module('sbAdminApp')
+	.directive('header',function(){
+		return {
+        templateUrl:'scripts/directives/header/header.html',
+        restrict: 'E',
+        replace: true,
+    	}
+	});
+
+
